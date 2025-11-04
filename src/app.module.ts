6db7a374de36362c7dfa67b4191c25f3c95948b5
Rafael Mata <83139503+Rafa-Mt@users.noreply.github.com>
2025-11-04 10:55:30 -0400

    chore: installed libraries and prepared directories

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { UsersController } from '@/controllers/users/users.controller';
+import { UsersService } from '@/services/users/users.service';
+import { UsersModule } from '@/modules/users/users.module';
+import { ClientsModule } from '@/modules/clients/clients.module';
+import { ClientsService } from '@/services/clients/clients.service';
+import { ClientsController } from '@/controllers/clients/clients.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ClientsModule],
+  controllers: [AppController, UsersController, ClientsController],
+  providers: [AppService, UsersService, ClientsService],
 })
 export class AppModule {}
